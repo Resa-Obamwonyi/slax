306d92e2f1bafc7083c86c4d35850fa2116928e4
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2025-02-11 08:36:57 +0100

    add ChatRoomLive.Edit

--- lib/slax/chat.ex
+++ lib/slax/chat.ex
@@ -4,7 +4,28 @@ defmodule Slax.Chat do
 
   import Ecto.Query
 
+
+  def get_room!(id) do
+    Repo.get!(Room, id)
+  end
+
   def list_rooms do
     Repo.all(from Room, order_by: [asc: :name])
+  end
+
+  def change_room(room, attrs \\ %{}) do
+    Room.changeset(room, attrs)
+  end
+
+  def create_room(attrs) do
+    %Room{}
+    |> Room.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_room(%Room{} = room, attrs) do
+    room
+    |> Room.changeset(attrs)
+    |> Repo.update()
   end
 end
--- lib/slax/chat/room.ex
+++ lib/slax/chat/room.ex
@@ -3,8 +3,8 @@ defmodule Slax.Chat.Room do
   import Ecto.Changeset
 
   schema "rooms" do
-    field :name, :string
-    field :topic, :string
+    field(:name, :string)
+    field(:topic, :string)
 
     timestamps(type: :utc_datetime)
   end
@@ -13,6 +13,9 @@ defmodule Slax.Chat.Room do
   def changeset(room, attrs) do
     room
     |> cast(attrs, [:name, :topic])
-    |> validate_required([:name, :topic])
+    |> validate_required(:name)
+    |> validate_length(:name, max: 80)
+    |> validate_format(:name, ~r/\A[a-z0-9-]+\z/, message: "can only contain lowercase letters, numbers and dashes")
+    |> validate_length(:topic, max: 200)
   end
 end

479b3936a6ed3fcfe18bb137444741a702b69de6
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2025-02-15 10:38:23 +0100

    add messages

--- lib/slax_web/controllers/chat_room_live.ex
+++ lib/slax_web/controllers/chat_room_live.ex
@@ -1,8 +1,8 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
 
-  alias Slax.Chat.Room
   alias Slax.Chat
+  alias Slax.Chat.{Message, Room}
 
   def mount(_params, _session, socket) do
     rooms = Chat.list_rooms()
@@ -22,11 +22,37 @@ defmodule SlaxWeb.ChatRoomLive do
           List.first(rooms)
       end
 
-    {:noreply, assign(socket, room: room, hide_topic?: false, page_title: "#" <> room.name)}
+    messages = Chat.list_messages_in_room(room)
+
+    {:noreply,
+     assign(socket,
+       hide_topic?: false,
+       messages: messages,
+       page_title: "#" <> room.name,
+       room: room
+     )}
   end
 
   def handle_event("toggle-topic", _params, socket) do
     {:noreply, update(socket, :hide_topic?, &(!&1))}
+  end
+
+  attr(:message, Message, required: true)
+
+  defp message(assigns) do
+    ~H"""
+    <div class="relative flex px-4 py-3">
+      <div class="h-10 w-10 rounded shrink-0 bg-slate-300"></div>
+      <div class="ml-2">
+        <div class="-mt-1">
+          <.link class="text-sm font-semibold hover:underline">
+            <span>User</span>
+          </.link>
+          <p class="text-sm">{@message.body}</p>
+        </div>
+      </div>
+    </div>
+    """
   end
 
   def render(assigns) do
@@ -93,6 +119,9 @@ defmodule SlaxWeb.ChatRoomLive do
               </li>
           </ul>
         </div>
+      <div class="flex flex-col grow overflow-auto">
+        <.message :for={message <- @messages} message={message} />
+      </div>
       </div>
     """
   end
